d576adf0f791b4699fd322d2484f0581218fffce
Rick South <rick@natamus.com>
2023-12-12 00:32:07 +0100

    Placeable Blaze Rods source code for 1.19.2.

---
+++ Common/src/main/java/com/natamus/placeableblazerods/ModCommon.java
@@ -0,0 +1,19 @@
+package com.natamus.placeableblazerods;
+
+
+import com.natamus.placeableblazerods.data.Constants;
+import com.natamus.placeableblazerods.util.Reference;
+import net.minecraft.core.Registry;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+
+public class ModCommon {
+
+	public static void init() {
+		load();
+	}
+
+	private static void load() {
+
+	}
+}
