0c01a5b05151167dceb97041362a91f284659bc3
Rick South <rick@natamus.com>
2023-12-18 10:13:34 +0100

    (v3.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/placeableblazerods/util/Reference.java
+++ Common/src/main/java/com/natamus/placeableblazerods/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.placeableblazerods.util;
 public class Reference {
 	public static final String MOD_ID = "placeableblazerods";
 	public static final String NAME = "Placeable Blaze Rods";
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
