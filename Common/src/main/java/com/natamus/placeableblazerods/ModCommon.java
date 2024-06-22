5249cd1d2d1fa5083cdfe99b552141886d076fea
Rick South <rick@natamus.com>
2024-06-23 00:30:01 +0200

    (v3.5) Simplified registering blocks with Collective's helper functions.

--- Common/src/main/java/com/natamus/placeableblazerods/ModCommon.java
+++ Common/src/main/java/com/natamus/placeableblazerods/ModCommon.java
@@ -1,11 +1,14 @@
 package com.natamus.placeableblazerods;
 
 
+import com.natamus.collective.services.Services;
+import com.natamus.placeableblazerods.blocks.BlazeRodBlock;
 import com.natamus.placeableblazerods.data.Constants;
 import com.natamus.placeableblazerods.util.Reference;
-import net.minecraft.core.Registry;
-import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 
 public class ModCommon {
 
@@ -15,5 +18,13 @@ public class ModCommon {
 
 	private static void load() {
 
+	}
+
+	public static void registerAssets(Object modEventBusObject) {
+		Services.REGISTERBLOCK.registerBlockWithoutItem(modEventBusObject, new ResourceLocation(Reference.MOD_ID, "blaze_rod"), () -> new BlazeRodBlock(BlockBehaviour.Properties.of(Material.DECORATION).strength(0.0F).lightLevel((blockState) -> { return 14; }).sound(SoundType.WOOD).noOcclusion()), true);
+	}
+
+	public static void setAssets() {
+		Constants.BLAZE_ROD_BLOCK = (BlazeRodBlock)Services.REGISTERBLOCK.getRegisteredBlockWithoutItem(new ResourceLocation(Reference.MOD_ID, "blaze_rod"));
 	}
 }
