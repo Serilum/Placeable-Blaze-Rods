d2ba5a116ba4d27b2ef60d28d319474d52143b4a
Rick South <rick@natamus.com>
2024-06-23 00:30:12 +0200

    (v3.5) Simplified registering blocks with Collective's helper functions.

--- Fabric/src/main/java/com/natamus/placeableblazerods/ModFabric.java
+++ Fabric/src/main/java/com/natamus/placeableblazerods/ModFabric.java
@@ -2,28 +2,23 @@ package com.natamus.placeableblazerods;
 
 import com.natamus.collective.check.RegisterMod;
 import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
-import com.natamus.placeableblazerods.blocks.BlazeRodBlock;
-import com.natamus.placeableblazerods.data.Constants;
 import com.natamus.placeableblazerods.events.BlazeRodEvent;
 import com.natamus.placeableblazerods.util.Reference;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.phys.BlockHitResult;
 
 public class ModFabric implements ModInitializer {
-	
+
 	@Override
 	public void onInitialize() {
 		setGlobalConstants();
 		ModCommon.init();
+		ModCommon.registerAssets(null);
+		ModCommon.setAssets();
 
 		registerBlocks();
 		loadEvents();
@@ -32,9 +27,7 @@ public class ModFabric implements ModInitializer {
 	}
 
 	private void registerBlocks() {
-		Constants.BLAZE_ROD_BLOCK = new BlazeRodBlock(BlockBehaviour.Properties.of().strength(0.0F).lightLevel((blockState) -> { return 14; }).sound(SoundType.WOOD).noOcclusion());
 
-		Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(Reference.MOD_ID, "blaze_rod"),  Constants.BLAZE_ROD_BLOCK);
 	}
 
 	private void loadEvents() {
