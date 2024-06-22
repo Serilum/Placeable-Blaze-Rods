7075f40927730eee3a5f85ecb9c75085d1fbd4f6
Rick South <rick@natamus.com>
2024-06-23 00:30:16 +0200

    (v3.5) Simplified registering blocks with Collective's helper functions.

--- Forge/src/main/java/com/natamus/placeableblazerods/ModForge.java
+++ Forge/src/main/java/com/natamus/placeableblazerods/ModForge.java
@@ -1,47 +1,32 @@
 package com.natamus.placeableblazerods;
 
 import com.natamus.collective.check.RegisterMod;
-import com.natamus.placeableblazerods.blocks.BlazeRodBlock;
-import com.natamus.placeableblazerods.data.Constants;
 import com.natamus.placeableblazerods.forge.events.ForgeBlazeRodEvent;
 import com.natamus.placeableblazerods.util.Reference;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 @Mod(Reference.MOD_ID)
 public class ModForge {
-
-	public static final DeferredRegister<Block> BLOCKS_REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, Reference.MOD_ID);
-
-	public static final RegistryObject<Block> BLAZE_ROD_BLOCK_OBJECT = BLOCKS_REGISTER.register("blaze_rod", () -> new BlazeRodBlock(BlockBehaviour.Properties.of().strength(0.0F).lightLevel((p_235454_0_) -> { return 14; }).sound(SoundType.WOOD).noOcclusion()));
-
-	public static Block BLAZE_ROD_BLOCK;
 	
 	public ModForge() {
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
-		BLOCKS_REGISTER.register(modEventBus);
-
 		setGlobalConstants();
 		ModCommon.init();
+		ModCommon.registerAssets(modEventBus);
 
 		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-		Constants.BLAZE_ROD_BLOCK = (BlazeRodBlock)BLAZE_ROD_BLOCK_OBJECT.get();
-
         MinecraftForge.EVENT_BUS.register(new ForgeBlazeRodEvent());
+
+		ModCommon.setAssets();
 	}
 
 	private static void setGlobalConstants() {
--- NeoForge/src/main/java/com/natamus/placeableblazerods/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/placeableblazerods/ModNeoForge.java
@@ -1,44 +1,30 @@
 package com.natamus.placeableblazerods;
 
 import com.natamus.collective.check.RegisterMod;
-import com.natamus.placeableblazerods.blocks.BlazeRodBlock;
-import com.natamus.placeableblazerods.data.Constants;
 import com.natamus.placeableblazerods.neoforge.events.NeoForgeBlazeRodEvent;
 import com.natamus.placeableblazerods.util.Reference;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 @Mod(Reference.MOD_ID)
 public class ModNeoForge {
-
-	private static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Reference.MOD_ID);
-
-	public static final DeferredBlock<Block> BLAZE_ROD_BLOCK_OBJECT = BLOCKS.register("blaze_rod", () -> new BlazeRodBlock(BlockBehaviour.Properties.of().strength(0.0F).lightLevel((p_235454_0_) -> { return 14; }).sound(SoundType.WOOD).noOcclusion()));
-
-	public static Block BLAZE_ROD_BLOCK;
 	
 	public ModNeoForge(IEventBus modEventBus) {
 		modEventBus.addListener(this::loadComplete);
 
-		BLOCKS.register(modEventBus);
-
 		setGlobalConstants();
 		ModCommon.init();
+		ModCommon.registerAssets(modEventBus);
 
 		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-		Constants.BLAZE_ROD_BLOCK = (BlazeRodBlock)BLAZE_ROD_BLOCK_OBJECT.get();
-
 		NeoForge.EVENT_BUS.register(NeoForgeBlazeRodEvent.class);
+
+		ModCommon.setAssets();
 	}
 
 	private static void setGlobalConstants() {
