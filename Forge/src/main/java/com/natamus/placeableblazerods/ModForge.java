91468ab390a420707b563af8c7d8a4155227c1e9
Rick South <rick@natamus.com>
2024-08-09 08:11:33 +0200

    Placeable Blaze Rods source code for 1.21.1.

---
+++ Forge/src/main/java/com/natamus/placeableblazerods/ModForge.java
@@ -0,0 +1,40 @@
+package com.natamus.placeableblazerods;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.placeableblazerods.forge.events.ForgeBlazeRodEvent;
+import com.natamus.placeableblazerods.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+		ModCommon.registerAssets(modEventBus);
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+        MinecraftForge.EVENT_BUS.register(new ForgeBlazeRodEvent());
+
+		ModCommon.setAssets();
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
