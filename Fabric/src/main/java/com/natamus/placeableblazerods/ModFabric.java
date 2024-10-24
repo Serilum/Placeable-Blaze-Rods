0c1a06c000e88952390ddd4ff5ea419a25a0d18c
Rick South <rick@natamus.com>
2024-10-24 19:43:20 +0200

    Placeable Blaze Rods source code for 1.21.2.

---
+++ Fabric/src/main/java/com/natamus/placeableblazerods/ModFabric.java
@@ -0,0 +1,47 @@
+package com.natamus.placeableblazerods;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
+import com.natamus.placeableblazerods.events.BlazeRodEvent;
+import com.natamus.placeableblazerods.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+		ModCommon.registerAssets(null);
+		ModCommon.setAssets();
+
+		registerBlocks();
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void registerBlocks() {
+
+	}
+
+	private void loadEvents() {
+		CollectiveBlockEvents.BLOCK_RIGHT_CLICK.register((Level world, Player player, InteractionHand hand, BlockPos pos, BlockHitResult hitVec) -> {
+			return BlazeRodEvent.onBlockClick(world, player, hand, pos, hitVec);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
