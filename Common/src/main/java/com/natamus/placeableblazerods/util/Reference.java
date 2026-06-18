9bb773802d70202ee2116d5ee6d71ab7d5da68aa
Rick South <rick@natamus.com>
2026-06-18 23:32:14 +0200

    Updated Placeable Blaze Rods to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/placeableblazerods/util/Reference.java
+++ Common/src/main/java/com/natamus/placeableblazerods/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "placeableblazerods";
 	public static final String NAME = "Placeable Blaze Rods";
 	public static final String VERSION = "4.0";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
