0cacf7eb8f086402cc6ea3385caa1e53e338c09f
Rick South <rick@natamus.com>
2026-06-29 18:52:30 +0200

    (v4.1) (Forge) Fixed placing down blaze rods.

--- Common/src/main/java/com/natamus/placeableblazerods/util/Reference.java
+++ Common/src/main/java/com/natamus/placeableblazerods/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.placeableblazerods.util;
 public class Reference {
 	public static final String MOD_ID = "placeableblazerods";
 	public static final String NAME = "Placeable Blaze Rods";
-	public static final String VERSION = "4.0";
+	public static final String VERSION = "4.1";
 	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
